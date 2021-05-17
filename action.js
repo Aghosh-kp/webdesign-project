6b550ca7a0d5d201faa6197fe52645163935ef80
Aghosh-kp <84304653+Aghosh-kp@users.noreply.github.com>
2021-05-17 20:26:48 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,13 @@
+function red(){
+    document.body.style.background="red"
+
+}
+function green(){
+    document.body.style.background="green"
+
+}
+function yellow(){
+    document.body.style.background="yellow"
+
+}
+
